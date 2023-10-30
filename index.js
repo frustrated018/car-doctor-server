94559b6fe4435377a470edc9135c58a6a2613d4f
frustrated018 <niloyd018@gmail.com>
2023-10-30 10:56:12 +0600

    Successfully fetched indiviual service by id

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const app = express(); 
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = process.env.port || 5000;
-require('dotenv').config()
-
+require("dotenv").config();
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hq7fxqk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,25 +28,30 @@ async function run() {
 
     //Collections
 
-    const serviceCollection = client.db('carDoctorDB').collection('services');
-
+    const serviceCollection = client.db("carDoctorDB").collection("services");
 
     //Sending and Fetching data from DataBase
 
-    app.get('/services', async(req, res) =>{
+    //fetching all data regardless of id
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-
-    } )
-
-
-
+    // fetching individual data by id
+    app.get("/services/:id", async (req, res) => {
+      const idx = req.params.id;
+      const query = { _id: new ObjectId(idx) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -57,16 +59,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
-
-
-
-
-
-app.get('/', (req, res) =>{
-    res.send('Server is running...');
-})
-
-app.listen(port, () =>{
-    console.log(`Server is runnin on Port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is runnin on Port ${port}`);
+});
